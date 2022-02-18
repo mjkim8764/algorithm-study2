ff0e5e99abe789d5332571b5c8f580ed9ce16f5e
MJKIM <mjkim8764@gmail.com>
2022-02-18 22:35:28 +0900

    [Add] week2-Prob02

--- src/week2/Prob02.java
+++ src/week2/Prob02.java
@@ -1,43 +1,98 @@
 package week2;
 
 /*
-
-Incomplete
-
-*/
-
-
-
+    Week2.Prob02 : 소수 찾기
+    url : https://programmers.co.kr/learn/courses/30/lessons/42839
+ */
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Prob02 {
-    static HashSet<Integer> numbersSet = new HashSet<>();
 
-    public static int solution(String numbers) {
+    public static int solution(String numbers){
         int answer = 0;
+        int length = numbers.length();
 
-        permutation(numbers, "");
+        int[] intNumbers = new int[length];
+        LinkedList<Integer> list = new LinkedList<>();
+        HashSet<Integer> set = new HashSet<>();
+        int[] visited = new int[length];
+
+
+        // string -> int
+        for(int i=0; i<length; i++)
+            intNumbers[i] = numbers.charAt(i) - 48;
+
+        // DFS
+        for(int depth=0; depth<length; depth++){
+            DFS(depth, list, set, visited, length, intNumbers);
+        }
+
+        // 소수 판별
+        Iterator<Integer> ir = set.iterator();
+        while(ir.hasNext()) {
+            int sum = ir.next();
+            if(sum >= 2 && isPrime(sum)) {
+                // System.out.println(sum);
+                answer++;
+            }
+        }
+
         return answer;
     }
 
-    public static void permutation(String remain, String current){
-        if(!remain.equals("")) {
-            numbersSet.add(Integer.valueOf(remain));
+    // DFS
+    public static void DFS(int depth, LinkedList<Integer> list, HashSet<Integer> set, int[] visited, int length, int[] intNumbers){
+        if(depth == -1) {
+            // 중복 방지를 위해 set에 저장
+            set.add(makeSum(list));
+        } else {
+            for(int i=0; i<length; i++) {
+                if(visited[i] != 1) {
+                    list.add(intNumbers[i]);
+                    visited[i] = 1;
+                    DFS(depth - 1, list, set, visited, length, intNumbers);
+                    visited[i] = 0;
+                    list.removeLast();
+                }
+            }
         }
     }
 
-    public static boolean isPrime(int number){
-        return false;
+
+    // list에 저장된 숫자들로 sum을 구함
+    public static int makeSum(LinkedList<Integer> list) {
+        int sum = 0;
+        int pow = 0;
+
+        Iterator<Integer> ir = list.listIterator();
+        while (ir.hasNext())
+            sum += ir.next() * Math.pow(10, pow++);
+
+        return sum;
     }
 
+
+    // 소수 판별
+    public static boolean isPrime(int sum){
+        for(int i=2; i<=(int)(Math.sqrt(sum)); i++) {
+            if(sum % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
-        String numbers1 = "17";
-        String numbers2 = "011";
+        //String numbers1 = "17";
+        //String numbers2 = "011";
         String numbers3 = "317";
 
-        System.out.println(solution(numbers1));
-        System.out.println(solution(numbers2));
+        //System.out.println(solution(numbers1));
+        //System.out.println(solution(numbers2));
+        System.out.println(solution(numbers3));
     }
 
 }
