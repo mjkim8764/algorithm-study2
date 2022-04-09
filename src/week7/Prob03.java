5fd048ba82d645007c7036209da00bf4c7f6f343
MJKIM <mjkim8764@gmail.com>
2022-04-09 15:38:38 +0900

    Modify week7-prob03

--- src/week7/Prob03.java
+++ src/week7/Prob03.java
@@ -5,32 +5,59 @@ package week7;
     url : https://programmers.co.kr/learn/courses/30/lessons/42895
 */
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
-/*
 public class Prob03 {
     
     public static int solution(int N, int number) {
         int answer = 0;
-        int[][] resultSets = new int[10][40];
 
-        resultSets[0][0] = N;
+        // set[i] : i 자리로 표현할 수 있는 모든 값들을 저장하고 있는 set
+        HashSet<Integer>[] set = new HashSet[9];
 
-        int count = 1;
-        while(count <= 8) {
-            for(int i=1; i<N; i++) {
-                for(int j=0; j<i; j++){
-                   // resultSets[i][ (j-1) * 4 + 1] = resultSets[j]
-            }
-            count++;
+        // set 초기화
+        for(int i = 0; i <= 8; i++) {
+            set[i] = new HashSet<>();
         }
 
+        // 5, 55, 555... 등을 먼저 set에 넣음
+        for(int i = 1; i <= 8; i++) {
 
+            int sum = 0;
+            for(int j = 0; j <= i - 1; j++) {
+                sum += (N * Math.pow(10, j));
+            }
 
-        return answer;
+            set[i].add(sum);
+        }
+
+        if(N == number) {
+            return 1;
+        } else {
+            for(int i = 2; i <= 8; i++){
+
+                for(int j = 1; j < i; j++) {
+
+                    // set[i] = set[1 ~ i-1], set[i-1 ~ 1] 사칙연산
+                    for(int k : set[j]) {
+                         for(int l : set[i - j]) {
+                             set[i].add(k + l);
+                             set[i].add(k * l);
+                             set[i].add(k - l);
+                             if(l != 0)
+                                 set[i].add(k / l);
+                        }
+                    }
+                }
+
+                if(set[i].contains(number))
+                    return i;
+            }
+        }
+
+        return -1;
+
     }
-
-
 
     public static void main(String[] args) {
         int N1 = 5;
@@ -43,4 +70,3 @@ public class Prob03 {
         System.out.println(solution(N2, number2));
     }
 }
-*/
